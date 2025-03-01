7e55a433b411aaa42c0110bba3c76eac18a03227
Jesse L. Zamora <xtremekforever@gmail.com>
2025-02-28 19:49:10 -0500

    Add copy of static dispatch/foundation into tarball

--- deploy-to-sysroot.sh
+++ deploy-to-sysroot.sh
@@ -5,18 +5,14 @@ source swift-define
 echo "Install Dispatch to sysroot"
 cp -rf ${LIBDISPATCH_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
 
-if [ $LIBDISPATCH_STATIC_INSTALL_PREFIX ]; then
-    echo "Install Dispatch Static to sysroot"
-    cp -rf ${LIBDISPATCH_STATIC_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
-fi
+echo "Install Dispatch Static to sysroot"
+cp -rf ${LIBDISPATCH_STATIC_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
 
 echo "Install Foundation to sysroot"
 cp -rf ${FOUNDATION_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
 
-if [ $FOUNDATION_STATIC_INSTALL_PREFIX ]; then
-    echo "Install Foundation Static to sysroot"
-    cp -rf ${FOUNDATION_STATIC_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
-fi
+echo "Install Foundation Static to sysroot"
+cp -rf ${FOUNDATION_STATIC_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
 
 echo "Install XCTest to sysroot"
 cp -rf ${XCTEST_INSTALL_PREFIX}/* ${STAGING_DIR}/usr/
