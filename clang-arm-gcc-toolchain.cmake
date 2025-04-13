d27def1562d17a701abd233d3752a7aa89a6ad0f
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-04-13 10:20:06 +0200

    remove more unneeded

--- clang-arm-gcc-toolchain.cmake
+++ clang-arm-gcc-toolchain.cmake
@@ -17,11 +17,8 @@ execute_process(COMMAND ${ARM_GCC_C_COMPILER} --version
 string(REGEX MATCH "[0-9]+\.[0-9]+\.[0-9]+" ARM_GCC_VERSION ${ARM_GCC_VERSION})
 # set compiler triple
 set(triple ${TOOLCHAIN_TRIPLE})
-set(CMAKE_ASM_COMPILER clang)
 set(CMAKE_ASM_COMPILER_TARGET ${triple})
-set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
 set(CMAKE_C_FLAGS_INIT " -B${ARM_TOOLCHAIN_DIR}")
